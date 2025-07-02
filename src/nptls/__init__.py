6e40438cd87bed4f12cb25868558599c480784fe
defaultuser8848 <158047456+defaultuser8848@users.noreply.github.com>
2025-07-02 14:26:59 +0800

    Export ConnectInfo.

--- src/nptls/__init__.py
+++ src/nptls/__init__.py
@@ -2,4 +2,4 @@
 from .nptls import npTLSServer, npTLSClient, MessageType
 from .utils.notepaper import Notepaper
 
-__all__ = ["npTLSServer", "npTLSClient", "MessageType", "Notepaper"]
+__all__ = ["npTLSServer", "npTLSClient", "MessageType", "Notepaper","ConnectInfo"]
